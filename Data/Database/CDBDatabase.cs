de00a4746c46f30a0a7d9936a54cc7d5e4824aec
pikachuk <lamcilaktheo@gmail.com>
2019-12-17 21:25:31 +0100

    Removed useless files

---
+++ Data/Database/CDBDatabase.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace libRPG.Database
+{
+    public class CDBDatabase
+    {
+    }
+}
---
+++ Gameplay/Overworld/Character.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace libRPG.Gameplay.Overworld
+{
+    public class Character
+    {
+    }
+}
